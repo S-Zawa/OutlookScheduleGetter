62983d95d9bdf46361d176e82128aa0735dd03c6
S.Aizawa <showta4696@gmail.com>
2022-06-28 22:51:49 +0900

    refs #3 ScheduleItem追加

---
+++ src/OutlookScheduleGetter/OutlookScheduleGetter.Tests/Domains/Entities/ScheduleItemTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OutlookScheduleGetter.Domains.Entities;
+using Xunit;
+
+namespace OutlookScheduleGetter.Tests.Domains.Entities
+{
+    public class ScheduleItemTests
+    {
+        [Fact]
+        public void ToStringFormat()
+        {
+            var start = new DateTime(2022, 1, 1, 1, 11, 0);
+            var end = start.AddDays(1);
+            var scheduleItem = new ScheduleItem("テスト", start, end);
+            var toString = $"テスト:2022/01/01(土) 01:11 - 2022/01/02(日) 01:11";
+            Assert.Equal(toString, scheduleItem.ToString());
+        }
+    }
+}
--- src/OutlookScheduleGetter/OutlookScheduleGetter/Domains/AppSettings.cs
+++ src/OutlookScheduleGetter/OutlookScheduleGetter/Domains/AppSettings.cs
@@ -8,6 +8,11 @@ namespace OutlookScheduleGetter.Domains
     public class AppSettings
     {
         /// <summary>
+        /// 日付時刻フォーマット
+        /// </summary>
+        public static readonly string DateTimeFormat = "yyyy/MM/dd(ddd) HH:mm";
+
+        /// <summary>
         /// MailOrName
         /// </summary>
         public string? MailOrName { get; set; }
